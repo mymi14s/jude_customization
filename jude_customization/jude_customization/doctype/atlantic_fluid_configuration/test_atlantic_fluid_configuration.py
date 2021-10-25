51114b59f49064a1d6e393f85faf63dcc114bed7
mymi14s <mymi14s@hotmail.com>
2021-10-25 07:10:33 +0000

    Added config

---
+++ jude_customization/jude_customization/doctype/atlantic_fluid_configuration/test_atlantic_fluid_configuration.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Anthony Emmanuel, github.com/mymi14ss and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAtlanticFluidConfiguration(unittest.TestCase):
+	pass
